ffc86f78b37d12fd6d6a5cffe3034ef1f3e3674a
Николай Кагала <nikolay.kagala@e-legion.com>
2016-08-22 14:41:49 +0300

    Add AAOdnoklassnikiViewController

---
+++ objC_test_social_framework/AAOdnoklassnikiViewController.h
@@ -0,0 +1,13 @@
+//
+//  AAOdnoklassnikiViewController.h
+//  objC_test_social_framework
+//
+//  Created by Николай Кагала on 22/08/16.
+//  Copyright © 2016 E-legion. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AAOdnoklassnikiViewController : UIViewController
+
+@end
---
+++ objC_test_social_framework/AAOdnoklassnikiViewController.m
@@ -0,0 +1,17 @@
+//
+//  AAOdnoklassnikiViewController.m
+//  objC_test_social_framework
+//
+//  Created by Николай Кагала on 22/08/16.
+//  Copyright © 2016 E-legion. All rights reserved.
+//
+
+#import "AAOdnoklassnikiViewController.h"
+
+@interface AAOdnoklassnikiViewController ()
+
+@end
+
+@implementation AAOdnoklassnikiViewController
+
+@end
