9bc3a7578ae26fd37da8a973309769e71231aa95
Юрий Петухов <yuriy.petuhov@e-legion.com>
2016-08-23 15:21:42 +0300

    Added AALinkedInWebViewController

---
+++ objC_test_social_framework/Code/AALinkedInWebViewController.h
@@ -0,0 +1,13 @@
+//
+//  AALinkedInWebViewController.h
+//  objC_test_social_framework
+//
+//  Created by Юрий Петухов on 23/08/16.
+//  Copyright © 2016 E-legion. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AALinkedInWebViewController : UIViewController
+
+@end
---
+++ objC_test_social_framework/Code/AALinkedInWebViewController.m
@@ -0,0 +1,37 @@
+//
+//  AALinkedInWebViewController.m
+//  objC_test_social_framework
+//
+//  Created by Юрий Петухов on 23/08/16.
+//  Copyright © 2016 E-legion. All rights reserved.
+//
+
+#import "AALinkedInWebViewController.h"
+
+@interface AALinkedInWebViewController ()
+
+@end
+
+@implementation AALinkedInWebViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
